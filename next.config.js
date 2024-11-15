16959522d4f2a3548a3091f8722d196812b0b958
moehami <m.aljaaly@yahoo.co.uk>
2024-11-15 21:56:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 module.exports = {
-  experiments: {
+  reactStrictMode: true,
+  experimental: {
     topLevelAwait: true,
   },
 };
