2f96e9dd6e3c7bcff77b80575954cd15945b89e3
moehami <m.aljaaly@yahoo.co.uk>
2024-11-16 08:40:34 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Pets from '../components/Pets';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Adoption from '../components/Adoption';
-import PetName from '../components/PetNameGenerator';
+import PetName from '../components/PetNameGenerator2';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 
