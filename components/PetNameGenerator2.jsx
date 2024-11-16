ad00375aa1697f3252c197b3ea4317043ebb1cfb
moehami <m.aljaaly@yahoo.co.uk>
2024-11-16 11:48:10 +0200

    Update PetNameGenerator2.jsx

--- components/PetNameGenerator2.jsx
+++ components/PetNameGenerator2.jsx
@@ -151,8 +151,7 @@ const handleCopy = (name) => {
               </div>
             ))}
          <div className="relative"><button data-copy-to-clipboard-target="npm-install-copy-text" onClick={() => handleCopy(name)}
-                >
-                  {copiedName === name ? "Copied!" : "Copy"} className="absolute end-2.5 top-1/2 -translate-y-1/2 text-gray-900 dark:text-gray-400 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border-gray-200 border">
+             className="absolute end-2.5 top-1/2 -translate-y-1/2 text-gray-900 dark:text-gray-400 hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-600 dark:hover:bg-gray-700 rounded-lg py-2 px-2.5 inline-flex items-center justify-center bg-white border-gray-200 border">
             <span id="default-message" className="inline-flex items-center">
                 <svg class="w-3 h-3 me-1.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
                     <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z"/>
@@ -165,6 +164,7 @@ const handleCopy = (name) => {
                 </svg>
                 <span className="text-xs font-semibold text-blue-700 dark:text-blue-500">Copied</span>   
             </span>
+                             {copiedName === name ? "Copied!" : "Copy"} 
         </button> </div>
           </div>
         </div>
