902f0c1c3fd63c4b5cd7b1c7c0e2104eb1eef7e7
moehami <m.aljaaly@yahoo.co.uk>
2024-11-17 18:04:01 +0200

    Update generate-name.js

--- pages/api/generate-name.js
+++ pages/api/generate-name.js
@@ -18,7 +18,7 @@ console.log("Prompt:", prompt);
         'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
      
       body: JSON.stringify({
         prompt: '${prompt}',
