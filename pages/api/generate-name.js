b6004528d5f9ddae7e023a1a0f3a656f98203160
moehami <m.aljaaly@yahoo.co.uk>
2024-11-17 17:23:43 +0200

    Update generate-name.js

--- pages/api/generate-name.js
+++ pages/api/generate-name.js
@@ -16,7 +16,10 @@ console.log("Prompt:", prompt);
       headers: {
         'Authorization': `Bearer ${AI21_API_KEY}`,
         'Content-Type': 'application/json',
-      },
+        'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+     
       body: JSON.stringify({
         prompt: '${prompt}',
         numResults: 1,
