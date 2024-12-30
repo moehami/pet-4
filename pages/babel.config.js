27035a641714eddcb434c61f6aef664731629528
moehami <m.aljaaly@yahoo.co.uk>
2024-12-30 21:02:32 +0200

    Update By MOe

--- pages/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    presets: [
-      ['next/babel'], // Ensure Next.js preset
-      ['@babel/preset-react', { runtime: 'automatic' }] // Enforce React's automatic runtime
-    ]
-  };
-  
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Pets from '../components/Pets';
 import Hero from '../components/Hero';
 import Services from '../components/Services';
 import Adoption from '../components/Adoption';
+import PetName from '../components/PetNameGenerator2';
 import Newsletter from '../components/Newsletter';
 import Footer from '../components/Footer';
 import Layout from '../components/Layout';
@@ -12,6 +13,7 @@ const Home = () => {
     <Layout title="Home">
     <div className='max-w-[1440px] mx-auto overflow-hidden'>
       <Hero />
+      <PetName />
       <Pets />
       <Services />
       <Adoption />
