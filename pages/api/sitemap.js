0445f058887c10e7fdc098aebe845e2d8d50abe2
moehami <m.aljaaly@yahoo.co.uk>
2025-01-02 16:39:28 +0200

    Update By MOe

--- pages/api/sitemap.js
+++
@@ -1,9 +0,0 @@
-import { generateSitemap } from '/@/lib/utils/generateSitemap';
-
-export default async function handler(req, res) {
-  const sitemap = await generateSitemap();
-
-  res.setHeader('Content-Type', 'application/xml');
-  res.write(sitemap);
-  res.end();
-}
